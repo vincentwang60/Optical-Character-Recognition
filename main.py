5dc6134762a76f0ce43ba292b97567f73cd4f978
VIncent <vincent.wang.60@gmail.com>
2020-11-20 05:21:39 -0800

    working lines

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
 from PIL import Image
+from PIL import ImageFilter
 import random as r
 import math as m
 
-def convert_to_bw(im):#returns image and list of pixels
-    width,height = im.size #width = im.size[0], height = im.size[1]
-    copy = im.copy()
-    pix = copy.load() #pix 2d-array of rgb (rep as tuple ex [r,g,b])
-    avg = 0 #average pixel value of the full image
-    threshold = 0.8 #amount that avg is scaled by to determine what is excluded
-    sample_size = 50
-    for i in range(width):
-        for j in range(height):
-            avg += sum(pix[i,j])
-    avg = avg/(3*width*height)
-
-    print(avg)
-    for i in range(width):
-        for j in range(height):
-            if sum(pix[i,j])/3 < avg*threshold:#if below threshold, replace w black
-                pix[i,j] = (0,0,0)
-                black.append([i,j])
+def get_threshold(im): #determines the average value of a cropped image for convert_to_bw
+    width,height = im.size
+    pix = im.load()
+    out = 0
+    for w in range(width):
+        for h in range(height):
+            out += sum(pix[w,h])/3
+    return out/(width*height)
+def convert_to_bw(im,size):#returns image and list of pixels
+    im = im.filter(ImageFilter.GaussianBlur(radius=1))
+    #im.show()
+    pix = im.load()
+    new = Image.new(mode = "RGB", size = (im.size[0], im.size[1]))
+    new_pix = new.load()
+    width,height = im.size
+    local_w,local_h = [int(width/size),int(height/size)]
+    thresholds = []
+    for x in range(size):
+        t = []
+        for y in range(size):
+            t.append(get_threshold(im.crop((x*local_w,y*local_h,(x+1)*local_w,(y+1)*local_h))))
+        thresholds.append(t)
+    for x in range(width):
+        for y in range(height):
+            t = thresholds[int(x*size/width)][int(y*size/height)]
+            if sum(pix[x,y]) > 2.7*t:
+                new_pix[x,y] = (255,255,255)
             else:
-                pix[i,j] = (255,255,255)
-    return [copy,pix]
-
+                new_pix[x,y] = (0,0,0)
+    return new
 def find_angle(pix, samples):  # given black and white pixels and list of samples, returns angle to rotate by
     # to go r distance in the direction theta add r*cos(theta) to x val, and r*sin(theta) to y val
     # (100,100). (100+r*cos(30),100+r*sin(30))
@@ -48,39 +57,63 @@ def find_angle(pix, samples):  # given black and white pixels and list of sample
         samp_list[samp] += ang_bool_list[samp]
     elm_num = samp_list.index(max(samp_list))
     return angles[elm_num]
-
-def find_lines(im):
-    out = []
-    width,height = im.size
+def filter(dict,min): #removes lines that are too close to each other from initial find_lines
+    done = False
+    while not done:
+        done = True
+        list = []
+        for key in dict.keys():
+            list.append(key)
+        print('l',list)
+        for i in range(len(list)-1):
+            if list[i+1]-list[i]<min:
+                done = False
+                if list[i+1] in dict and list[i] in dict:
+                    if dict[list[i+1]]>dict[list[i]]:
+                        del dict[list[i]]
+                    else:
+                        del dict[list[i+1]]
+    return dict
+def find_lines(im,search,threshold): #finds lines from rotated bw image
+    out = {}
+    counts = []
+    new = Image.new(mode = "RGB", size = (im.size[0], im.size[1]))
+    new_pix = new.load()
     pix = im.load()
-    threshold = 0.02
-    check = True
-    black_found = False
-    skip = int(height/50)
-    increment = 0
-    for y in range(height):
-        black = 0
-        if not check:
-            if increment < skip:
-                increment += 1
+    width,height = im.size
+    for h in range(height):
+        count = 0
+        for w in range(width):
+            if pix[w,h] == (0,0,0):
+                count += 1
+        counts.append(count)
+        for i in range(count):
+            new_pix[i,h] = (255,0,0)
+    for i in range(len(counts)-2):
+        if counts[i] > counts[i+1] and counts[i+1] <= counts[i+2]:
+            top = 0
+            bot = height
+            if i-search+1 > 0:
+                top = i-search+1
+            if i+search + 2<height:
+                bot = i+search + 2
+            #print(top,bot,counts[top:bot],counts[i+1],i+1)
+            if counts[i+1] != 0:
+                if max(counts[top:bot])/counts[i+1] > threshold:
+                    out[i+1] = max(counts[top:bot])/counts[i+1]
             else:
-                check = True
-                increment = 0
-        for x in range(width):
-            if pix[x,y] == (0,0,0):
-                black += 1
-        if black/width > threshold:
-            black_found = True
-        elif check and black_found:
-            for x in range(width):
-                pix[x,y] = (255,0,255)
-            out.append(y)
-            check = False
-            black_found = False
+                out[i+1] = max(counts[top:bot])
+    print('o',out)
+    out = filter(out,int(height/50))
+    print('o',out)
+    for el in out:
+      for x in range(width):
+          pix[x,el] = (255,0,0)
+          new_pix[x,el] = (255,255,255)
     im.show()
-    return out
-
-def make_histogram(im,lines):
+    new.show()
+    return list(out.keys())
+def make_histogram(im,lines):#returns 2d-array which is histogram of each line, also show convenient picture
     out = []
     new = Image.new(mode = "RGB", size = (im.size[0], im.size[1]))
     new_pix = new.load()
@@ -98,12 +131,32 @@ def make_histogram(im,lines):
         out.append(list)
     new.show()
     return out
+def segment_line(line): #doesn't work, tries to get words from line
+    out = []
+    avg = int(sum(line)/len(line))
+    open = False
+    top,bot = [0,0]
+    for h in range(len(line)):
+        if not open and line[h] < avg:
+            top = h
+            open = True
+        if open and line[h] > avg:
+            bot = h
+            #if bot-top > len(line)/50:
+            out.append(int((top+bot)/2))
+            open = False
+    return out
 
-black = []
 im = Image.open("sample2.jpeg")
-copy,pix = convert_to_bw(im)
-samples = r.sample(black,50) #Random Sample of black pixels
-rotated = copy.rotate(-1)#find_angle(pix,samples))
-lines = find_lines(rotated)
+width,height = im.size
+rotated = convert_to_bw(im,10).rotate(-1,fillcolor = 'white')#rotated manually for now
+lines = find_lines(rotated,int(height/100),int(height/100))
 hist = make_histogram(rotated,lines)
-print(lines)
+for i in range(4):
+    out = segment_line(hist[i])
+    line_image = rotated.crop((0,lines[i],width,lines[i+1]))
+    line_pix = line_image.load()
+    for o in out:
+        for h in range(line_image.size[1]):
+            line_pix[o,h] = (255,0,0)
+    line_image.show()
