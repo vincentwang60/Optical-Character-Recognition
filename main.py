802f3e941f1d618f74ec4e27aba79226b7788b8e
VIncent <vincent.wang.60@gmail.com>
2020-11-19 21:00:39 -0800

    asdf

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from PIL import Image
 
 im = Image.open("sample.jpg")
-pix = im.load()
-width,height = im.size
-print(width,height)
-print(pix[1,1])
-print('amari can you see this')
+width,height = im.size #width = im.size[0], height = im.size[1]
+copy = im.copy()
+pix = copy.load() #pix 2d-array of rgb (rep as tuple ex [r,g,b])
+avg = 0 #average pixel value of the full image
+threshold = 0.8 #amount that avg is scaled by to determine what is excluded
+
+for i in range(width):
+    for j in range(height):
+        avg += sum(pix[i,j])
+avg = avg/(3*width*height)
+
+print(avg)
+for i in range(width):
+    for j in range(height):
+        if sum(pix[i,j])/3 < avg*threshold:#if below threshold, replace w black
+            pix[i,j] = (0,0,0)
+        else:
+            pix[i,j] = (255,255,255)
+rotated = copy.rotate(-5)
+copy.show()
+rotated.show()
